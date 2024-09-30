55f2432c3fddeafeb81779b6f17b0cf68c9b1c16
Simeon Manolov <s.manolloff@gmail.com>
2024-09-30 17:33:13 +0300

    initial commit

---
+++ macos-arm64.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eux
+
+cd pytorch
+pip install cmake ninja
+pip install -r requirements.txt
+
+mkdir -p install
+
+scripts/build_local.sh \
+      -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
+      -DBUILD_LITE_INTERPRETER=ON
+
+cmake -P build/cmake_install.cmake
