17dba0f99e23d8ee4f6c80ae54c2d68cc985e812
Simeon Manolov <s.manolloff@gmail.com>
2024-09-30 18:25:52 +0300

    fix

--- macos-arm64.sh
+++ macos-arm64.sh
@@ -2,10 +2,9 @@
 
 set -eux
 
-cd pytorch
+cd $PYTORCH_DIR
 pip install cmake ninja
 pip install -r requirements.txt
-
 mkdir -p install
 
 scripts/build_local.sh \
@@ -13,3 +12,4 @@ scripts/build_local.sh \
       -DBUILD_LITE_INTERPRETER=ON
 
 cmake -P build/cmake_install.cmake
+tar --create --xz --file "$ARCHIVE_FILE" -C pytorch/install .
