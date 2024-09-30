232ec771cd3084d7db439dccddf1dd5484abb475
Simeon Manolov <s.manolloff@gmail.com>
2024-10-01 01:19:50 +0300

    try windows

--- linux-x64.sh
+++ linux-x64.sh
@@ -7,8 +7,15 @@ mkdir -p install
 
 pip install cmake ninja
 pip install -r requirements.txt
+pip install mkl-static mkl-include
 
-USE_CUDA=0 USE_DISTRIBUTED=0 BUILD_TEST=0 python setup.py develop
+export \
+  USE_CUDA=0
+  USE_DISTRIBUTED=0 \
+  BUILD_TEST=0 \
+  _GLIBCXX_USE_CXX11_ABI=1
+
+python setup.py develop
 
 cmake -D CMAKE_INSTALL_PREFIX=build/install -P build/cmake_install.cmake
 
