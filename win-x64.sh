8a741f5bd643b19ab7be4b6801a92f0bdbde8835
Simeon Manolov <s.manolloff@gmail.com>
2025-10-01 15:03:19 +0300

    .

--- win-x64.sh
+++ win-x64.sh
@@ -43,6 +43,7 @@ pip install -r requirements.txt
 export BUILD_TEST=0
 export USE_CUDA=0
 export USE_DISTRIBUTED=1    # bug
+export INTERN_DISABLE_ONNX=1
 export USE_GLOO=1
 export USE_FBGEMM=0  # depends on openmp; also causes asmjit build
 export USE_KINETO=0
