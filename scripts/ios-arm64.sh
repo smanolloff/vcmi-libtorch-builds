9b057d557af5e44de0f707beffba1d883d711daf
Simeon Manolov <s.manolloff@gmail.com>
2025-10-02 13:36:15 +0300

    update macos-x64 to runner to actually use intel; try Xcode gen for ios

--- scripts/ios-arm64.sh
+++ scripts/ios-arm64.sh
@@ -5,13 +5,13 @@ set -eux
 cd pytorch
 python -m venv .venv
 . .venv/bin/activate
-pip install cmake ninja
+pip install cmake #ninja
 pip install -r requirements.txt
 
 # build_ios.sh *must* be used for ios builds
 
 args=(
-  -G Ninja  # 'make' not available on macos runners
+  -G Xcode  # 'make' not available on macos runners
 )
 
 BUILD_LITE_INTERPRETER=1 scripts/build_ios.sh  "${args[@]}"
