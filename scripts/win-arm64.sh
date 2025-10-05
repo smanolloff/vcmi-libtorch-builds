f766f3b0b0dfaa2be9fe84901ac3677f9f8f45c0
Simeon Manolov <s.manolloff@gmail.com>
2025-10-05 17:43:02 +0300

    add android-x64 and windows-arm64

--- scripts/win-arm64.sh
+++ scripts/win-arm64.sh
@@ -2,28 +2,39 @@
 
 set -eux
 
+# 7z produces .txz archives
+if [ "$OUTPUT_ARCHIVE_FILE" != "${OUTPUT_ARCHIVE_FILE%.txz}.txz" ]; then
+  echo "Expected OUTPUT_ARCHIVE_FILE to end with .txz: $OUTPUT_ARCHIVE_FILE" >&2
+  exit 1
+fi
+
 cd pytorch
+
 python -m venv .venv
 . .venv/Scripts/activate
 pip install cmake ninja
 pip install -r requirements.txt
 
-# build_local *may* be used for mac builds
-# (and seems to produce smaller binaries compared to setup.py)
+# MKL is intel-specific
+# pip install mkl-static mkl-include
 
-args=(
-  -DCMAKE_INSTALL_PREFIX=libtorch  # must be available during build
-  -DBUILD_LITE_INTERPRETER=0  # causes errors (undefined symbols)
-  -DBUILD_PYTHON=0
-  -DBUILD_TEST=0
-  -DUSE_CUDA=0
-  -DUSE_DISTRIBUTED=0
-  -DUSE_LITE_INTERPRETER_PROFILER=0
-  -DUSE_KINETO=0
-  -DUSE_FBGEMM=0
-)
+export BUILD_TEST=0
+export USE_CUDA=0
+export USE_DISTRIBUTED=0
+export USE_FBGEMM=0  # depends on openmp; also causes asmjit build
+export USE_KINETO=0
+export USE_MPI=0
+export USE_MKL=0  # MKL is intel-specific
+export USE_MKLDNN=0
+export USE_NUMPY=0
+export USE_OPENMP=0  # openmp slows down inference
 
+# XXX: build_local *must not* be used for windows builds => use python setup.py
 python setup.py develop
+
 cmake -D CMAKE_INSTALL_PREFIX=libtorch -P build/cmake_install.cmake
 
-tar --create --xz --file "../$OUTPUT_ARCHIVE_FILE" -C build_mac libtorch
+TAR_FILE="${OUTPUT_ARCHIVE_FILE%.*}.tar"
+7z a -ttar "${TAR_FILE}" "libtorch"
+7z a -txz "$OUTPUT_ARCHIVE_FILE" "${TAR_FILE}"
+mv "$OUTPUT_ARCHIVE_FILE" ..
