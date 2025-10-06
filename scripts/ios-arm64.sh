7eec2388fded4d8bd4a715dbca09aa2c55f79b2e
Simeon Manolov <s.manolloff@gmail.com>
2025-10-06 10:48:50 +0300

    set deployment target for osx

--- scripts/ios-arm64.sh
+++ scripts/ios-arm64.sh
@@ -10,8 +10,18 @@ pip install -r requirements.txt
 
 export BUILD_LITE_INTERPRETER=1
 
+# As per VCMI's CMakeLists
+export MACOSX_DEPLOYMENT_TARGET=12.0
+export IOS_DEPLOYMENT_TARGET=12.0
+
 args=(
   -DCMAKE_MAKE_PROGRAM="$(which make)"    # prevents 'No rule to make target `install''
+  -DMACOSX_DEPLOYMENT_TARGET=12.0
+
+  # TODO: try only with env vars or only with cmake args
+  -DIOS_DEPLOYMENT_TARGET=$IOS_DEPLOYMENT_TARGET
+  -DMACOSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET
+
 )
 
 # build_ios.sh *must* be used for ios builds
