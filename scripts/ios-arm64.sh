1cc7319db276a14b515cb69448a4e4d96be82afe
Simeon Manolov <s.manolloff@gmail.com>
2025-10-06 10:59:34 +0300

    try w/o env vars

--- scripts/ios-arm64.sh
+++ scripts/ios-arm64.sh
@@ -10,18 +10,15 @@ pip install -r requirements.txt
 
 export BUILD_LITE_INTERPRETER=1
 
-# As per VCMI's CMakeLists
-export MACOSX_DEPLOYMENT_TARGET=12.0
-export IOS_DEPLOYMENT_TARGET=12.0
 
 args=(
   -DCMAKE_MAKE_PROGRAM="$(which make)"    # prevents 'No rule to make target `install''
+
+  # As per VCMI's CMakeLists
+  # XXX: setting those as env vars as well results in an error?
+  #      building for 'macOS', but linking in dylib (...) built for 'iOS'
   -DMACOSX_DEPLOYMENT_TARGET=12.0
-
-  # TODO: try only with env vars or only with cmake args
-  -DIOS_DEPLOYMENT_TARGET=$IOS_DEPLOYMENT_TARGET
-  -DMACOSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET
-
+  -DIOS_DEPLOYMENT_TARGET=12.0
 )
 
 # build_ios.sh *must* be used for ios builds
